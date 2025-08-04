a5e89bcf8f1879edef448ac4142fa83a2880923a
rajnish.oss <rajnishpandey844@gmail.com>
2025-08-04 14:15:31 +0530

    m2

--- app/page.tsx
+++ app/page.tsx
@@ -222,30 +222,30 @@ export default function TravelDiscoveryLanding() {
               <p className="text-gray-400">Find places that match your soul</p>
             </div>
             <div className="flex gap-8">
-              <a
+              <link
                 href="#"
                 className="text-gray-400 hover:text-white transition-colors"
               >
                 About
-              </a>
-              <a
+              </link>
+              <link
                 href="#"
                 className="text-gray-400 hover:text-white transition-colors"
               >
                 Contact
-              </a>
-              <a
+              </link>
+              <link
                 href="#"
                 className="text-gray-400 hover:text-white transition-colors"
               >
                 Instagram
-              </a>
-              <a
+              </link>
+              <link
                 href="#"
                 className="text-gray-400 hover:text-white transition-colors"
               >
                 Terms
-              </a>
+              </link>
             </div>
           </div>
           <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-400">
