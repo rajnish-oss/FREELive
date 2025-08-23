d40c9d5337d23adf92658690114107e0a099fb95
rajnish.oss <rajnishpandey844@gmail.com>
2025-08-23 08:10:59 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
